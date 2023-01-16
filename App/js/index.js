eb78c48227b3cfd78a135ea3e635949ffd877fcd
nickantonaccio <nick@guitarz.org>
2023-01-15 21:18:54 -0500

    Updated by CrossUI No-Code App Builder

--- App/js/index.js
+++ App/js/index.js
@@ -19,7 +19,22 @@ xui.Class('App', 'xui.Module',{
         iniComponents : function(){
             // [[Code created by CrossUI RAD Studio
             var host=this, children=[], append=function(child){children.push(child.get(0));};
-
+            
+            append(
+                xui.create("Module.test", "xui.Module")
+                .setHost(host,"module_test1")
+            );
+            
+            append(
+                xui.create("Module.test", "xui.Module")
+                .setHost(host,"module_test4")
+            );
+            
+            append(
+                xui.create("Module.test", "xui.Module")
+                .setHost(host,"module_test5")
+            );
+            
             return children;
             // ]]Code created by CrossUI RAD Studio
         },
